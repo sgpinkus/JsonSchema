d29f0f187fca169fe41753ef998c77c97ec0948e
sam-at-github <sgpinkus@gmail.com>
2015-11-10 22:30:15 +1100

    Add test script.

--- docs/README.md
+++ docs/README.md
@@ -4,7 +4,7 @@ Draft v4 compliant JSON Schema validator for PHP.
   * Complete support for `$refs`, and the `id` keyword except for the next point.
   * No support for the "inline dereferencing" option.
   * Simple design. In particular, the separation of code concerned with loading JSON, and JSON Reference, and code concerned with validation.
-  * Simple interface for validation - doesn't expose the user to more thatn a couple of classes for the main use case - validation.
+  * Simple interface for validation - doesn't expose the user to more than a couple of classes for the main use case - validation.
   * Easily extensible with custom constraints.
   * Draft 4 compatible only.
   * No support for the hypermedia validation / semantic validation or whatever it is (the 3rd part). Should be easy to add support for this later.
---
+++ tests-informal/ValidateFile.php
@@ -0,0 +1,39 @@
+<?php
+require_once '../loader.php';
+use JsonDocs\JsonDocs;
+use JsonDocs\JsonLoader;
+use JsonDocs\Uri;
+use JsonSchema\JsonSchema;
+use JsonSchema\Constraint\ValidationError;
+
+function main($argc, $argv) {
+  sizeof($argv) == 3 or die(usage());
+  $schema_file = make_path($argv[1]) or die("Invalid schema file\n");
+  $target = file_get_contents($argv[2]) or die("Invalid JSON file\n");
+  print "$schema_file $target\n";
+  $jsonDocs = new JsonDocs(new JsonLoader());
+  $doc = $jsonDocs->get(new Uri($schema_file));
+  $schema = new JsonSchema($doc);
+  $valid = $schema->validate($target);
+  if($valid instanceof ValidationError) {
+    print "Validation Failed:\n";
+    print $valid->getName() . ": " .$valid->getMessage() ."\n";
+  }
+  else {
+    print "OK. $pointer.\n";
+  }
+}
+
+function make_path($file) {
+  $file = realpath($file);
+  if(!$file) {
+    return false;
+  }
+  return "file://$file";
+}
+
+function usage() {
+  return "Usage: " . basename(__FILE__) . " <schemafile> <targetjsonfile>\n";
+}
+
+main($argc, $argv);
