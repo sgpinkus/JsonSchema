d1b4002b55250e9c1d35585965ea2546efec9e5a
sam-at-github <sgpinkus@gmail.com>
2015-12-20 13:22:50 +1100

    Rework Properties constraint to be consistent with spec.

--- Constraint/PropertiesConstraint.php
+++ Constraint/PropertiesConstraint.php
@@ -5,21 +5,22 @@ use JsonSchema\Constraint\Constraint;
 use JsonSchema\Constraint\Exception\ConstraintParseException;
 
 /**
- * The properties constraint.
- * Note patternProperties is implemented as a completely separate constraint.
- * @see ItemsConstraint.php. This is ~C&P from properties constraint.
+ * All three properties constraints.
+ * These three constraints are interelated so cannot be addressed independently - bit of a PITA.
  */
 class PropertiesConstraint extends Constraint
 {
   private $properties;
+  private $patternProperties;
   private $additionalProperties;
 
   /**
    * @input $properties assoc array of EmptyConstraint.
    * @input $additionalProperties Mixed either an EmptyConstraint or bool.
    */
-  public function __construct(array $properties, $additionalProperties = true) {
+  public function __construct(array $properties = [], array $patternProperties = [], $additionalProperties = true) {
     $this->properties = $properties;
+    $this->patternProperties = $patternProperties;
     $this->additionalProperties = $additionalProperties;
   }
 
@@ -27,50 +28,75 @@ class PropertiesConstraint extends Constraint
    * @override
    */
   public static function getName() {
-    return 'properties';
+    return '[properties, patternProperties, additionalProperties]';
   }
 
   /**
    * Ensure properties of object match given constraints.
    * Properties only apply if the property is defined on the target.
+   * Minor issue: in continueMode first error becomes the parent error, while following errors push onto it.
    * @override
    */
   public function validate($doc, $context) {
     $valid = true;
     if(is_object($doc)) {
       $arrayDoc = (array)$doc;
-      if($this->additionalProperties == false && sizeof($arrayDoc) > sizeof($this->properties)) {
-        $valid = new ValidationError($this, "No additional properties allowed", $context);
-      }
-      else {
-        foreach($this->properties as $i => $constraint) {
-          if(isset($arrayDoc[$i])) {
-            $validation = $constraint->validate($arrayDoc[$i], $i);
-            if($validation instanceof ValidationError) {
-              if($valid === true) {
-                $valid = new ValidationError($this, "One or more properties failed to validate.", $context);
-              }
-              if(!$this->continueMode()) {
-                break;
-              }
-              $valid->addChild($validation);
+      foreach($this->properties as $i => $constraint) {
+        if(isset($arrayDoc[$i])) {
+          $validation = $constraint->validate($arrayDoc[$i], $i);
+          unset($arrayDoc[$i]);
+          if($validation instanceof ValidationError) {
+            if($valid === true) {
+              $valid = new ValidationError($this, "One or more properties failed to validate.", $context);
+            }
+            $valid->addChild($validation);
+            if(!$this->continueMode()) {
+              break;
             }
           }
         }
       }
-      // If we reach here additionalProperties are allowed, but they must pass additionalProperties constraint if specified.
-      if($valid == true && is_object($this->additionalProperties)) {
-        foreach($arrayDoc as $i => $value) {
-          if(!isset($this->properties[$i])) {
+      if($valid === true || $this->continueMode()) {
+        foreach($this->patternProperties as $pattern => $constraint) {
+          foreach($arrayDoc as $docKey => $docItem) {
+            if(preg_match($pattern, $docKey)) {
+              $validation = $constraint->validate($docItem, $pattern);
+              unset($arrayDoc[$docKey]);
+              if($validation instanceof ValidationError) {
+                if($valid === true) {
+                  $valid = new ValidationError($this, "One or more pattern properties failed to validate.", $context);
+                }
+                $valid->addChild($validation);
+                if(!$this->continueMode()) {
+                  break;
+                }
+              }
+            }
+          }
+        }
+      }
+      if($valid === true || $this->continueMode()) {
+        if(is_object($this->additionalProperties)) {
+          foreach($arrayDoc as $i => $value) {
             $validation = $this->additionalProperties->validate($arrayDoc[$i], $i);
             if($validation instanceof ValidationError) {
               if($valid === true) {
-                $valid = new ValidationError($this, "One or more additional items failed validation.", $context);
+                $valid = new ValidationError($this, "One or more pattern properties failed to validate.", $context);
               }
+              $valid->addChild($validation);
               if(!$this->continueMode()) {
                 break;
               }
-              $valid->addChild($validation);
+            }
+          }
+        }
+        elseif($this->additionalProperties === false) {
+          if(sizeof($arrayDoc) > 0) {
+            if($valid === true) {
+              $valid = new ValidationError($this, "Additional properties found.", $context);
+            }
+            else {
+              $vali->addChild(new ValidationError($this, "Additional properties found.", $context));
             }
           }
         }
@@ -80,24 +106,76 @@ class PropertiesConstraint extends Constraint
   }
 
   /**
+   * We only care about the context not doc here.
+   * properties, additionalProperties, or patternProperties must be set in context.
    * @override
    */
   public static function build($doc, $context = null) {
-    $constraints = null;
+    $constraints = [];
+    $properties = null;
+    $patternProperties = null;
+    $additionalProperties = null;
 
-    if(!is_object($doc)) {
-      throw new ConstraintParseException('The value MUST be either an object.');
+    if(isset($context->properties)) {
+      $properties = self::buildPropertyConstraints($context->properties);
     }
-    if(isset($context->additionalProperties) && !(is_bool($context->additionalProperties) || is_object($context->additionalProperties))) {
-       throw new ConstraintParseException('The value of "additionalProperties" MUST be either a boolean or an object.');
+    if(isset($context->patternProperties)) {
+      $patternProperties = self::buildPatternPropertyConstraints($context->patternProperties);
+    }
+    if(isset($context->additionalProperties)) {
+      $additionalProperties = self::buildAdditionalPropertyConstraints($context->additionalProperties);
+    }
+    if(!(isset($properties) || isset($patternProperties) || isset($additionalProperties))) {
+      throw new ConstraintParseException('One of properties, additionalProperties, or patternProperties must be set for PropertiesConstraint.');
+    }
+    return new static($properties, $patternProperties, $additionalProperties);
+  }
+
+  /**
+   *
+   */
+  public static function buildPropertyConstraints($properties) {
+    $constraints = [];
+    if(!is_object($properties)) {
+      throw new ConstraintParseException('The value MUST be an object.');
     }
     foreach($doc as $key => $value) {
       $constraints[$key] = EmptyConstraint::build($value);
     }
-    $additionalProperties = isset($context->additionalProperties) ? $context->additionalProperties : true;
-    if(is_object($additionalProperties)) {
-      $additionalProperties = EmptyConstraint::build($additionalProperties);
+    return $constraints;
+  }
+
+  /**
+   *
+   */
+  public static function buildPatternPropertyConstraints($properties) {
+    $constraints = [];
+    if(!is_object($properties)) {
+      throw new ConstraintParseException('The value MUST be an object.');
     }
-    return new static($constraints, $additionalProperties);
+    foreach($doc as $key => $value) {
+      if(preg_match($key, "0") === false) {
+        throw new ConstraintParseException("The keys of 'patternProperties' must be valid regexps.");
+      }
+      $constraints[$key] = EmptyConstraint::build($value);
+    }
+    return $constraints;
+  }
+
+  /**
+   *
+   */
+  public static function buildAdditionalPropertyConstraints($additionalProperties) {
+    $constraint = false;
+    if(is_bool($additionalProperties)) {
+      $constraint = $additionalProperties;
+    }
+    elseif(is_object($properties)) {
+      $constraint = EmptyConstraint::build($additionalProperties);
+    }
+    else {
+      throw new ConstraintParseException('The value MUST be either a boolean or object.');
+    }
+    return $constraint;
   }
 }
